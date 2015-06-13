0108070e57b5e116766ed05457947b8d4b676212
Vitalii Maklai <vitalii.maklai@gmail.com>
2015-06-13 01:04:16 -0400

    Project skeleton

---
+++ src/LoadRunnerScriptShrinker.UI.AddIn/Commands/StartupCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using ICSharpCode.Core;
+
+namespace MyLoadTest.LoadRunnerScriptShrinker.UI.AddIn.Commands
+{
+    /// <summary>
+    ///     Represents the command that is executed automatically when IDE starts.
+    /// </summary>
+    public sealed class StartupCommand : AbstractMenuCommand
+    {
+        #region Public Methods
+
+        /// <summary>
+        ///     Invokes the command.
+        /// </summary>
+        public override void Run()
+        {
+            //// TODO [vmcl] Implement StartupCommand.Run
+        }
+
+        #endregion
+    }
+}
